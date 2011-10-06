9d0972ba847a6abd316ca0b32c3538dbe7c63406
keithnlarsen <keithnlarsen@gmail.com>
2011-10-06 11:15:55 -0600

    Finished writing a test that actually tests work flow on a view

--- src/test/java/ca/shawmedia/globaltv/MyActivityTest.java
+++ src/test/java/ca/shawmedia/globaltv/MyActivityTest.java
@@ -1,27 +1,40 @@
 package ca.shawmedia.globaltv;
 
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(RobolectricTestRunner.class)
 public class MyActivityTest {
+    MyActivity_ _activity;
+
+    @Before
+    public void setup() throws Exception {
+        _activity = new MyActivity_();
+        _activity.onCreate(null);
+    }
 
     @Test
     public void shouldOpenMyActivity() throws Exception {
-        String appName = new MyActivity().getResources().getString(R.string.app_name);
-//        assertThat(appName, equalTo("MyActivity"));
-
+        String appName = _activity.getResources().getString(R.string.app_name);
         Assert.assertEquals(appName, "Hello, Android");
     }
 
     @Test
-    public void shouldSetTextWhenButtonClicked() throws Exception {
-        String appName = new MyActivity().getResources().getString(R.string.app_name);
-        String textfield = new MyActivity().getResources().getString(R.id.myTextView);
-
-        Assert.assertEquals(appName, "Hello, Android");
+    public void shouldSetTextViewValueWhenButtonClicked() throws Exception {
+        TextView actualTextView = (TextView)_activity.findViewById(R.id.myTextView);
+        Button actualButton = (Button)_activity.findViewById(R.id.myButton);
+        EditText actualInput = (EditText)_activity.findViewById(R.id.myInput);
+        // Check that when the view starts up the text is set to the value defined in hello
+        Assert.assertEquals(_activity.getResources().getString(R.string.hello), actualTextView.getText());
+        // Set some text, and perform click and verify that the text changes in the TextView
+        actualInput.setText("testing");
+        actualButton.performClick();
+        Assert.assertEquals("Hello testing", actualTextView.getText());
     }
 }
