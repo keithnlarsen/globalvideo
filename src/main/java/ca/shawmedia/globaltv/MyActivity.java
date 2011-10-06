e0c878025c69e7f0beab95ab49e6b2668b42301a
keithnlarsen <keithnlarsen@gmail.com>
2011-10-06 00:49:30 -0600

    Getting it working with androidannotations

--- src/main/java/ca/shawmedia/globaltv/MyActivity.java
+++ src/main/java/ca/shawmedia/globaltv/MyActivity.java
@@ -1,13 +1,24 @@
 package ca.shawmedia.globaltv;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.googlecode.androidannotations.annotations.Click;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
 
+@EActivity(R.layout.main)
 public class MyActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+    @ViewById
+    EditText myInput;
+
+    @ViewById(R.id.myTextView)
+    TextView textView;
+
+    @Click
+    void myButton() {
+         String name = myInput.getText().toString();
+         textView.setText("Hello " + name);
     }
 }
--- src/test/java/ca/shawmedia/globaltv/MyActivityTest.java
+++ src/test/java/ca/shawmedia/globaltv/MyActivityTest.java
@@ -1,5 +1,6 @@
 package ca.shawmedia.globaltv;
 
+import android.widget.TextView;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import junit.framework.Assert;
 import org.junit.Test;
@@ -9,9 +10,17 @@ import org.junit.runner.RunWith;
 public class MyActivityTest {
 
     @Test
-    public void shouldHaveHappySmiles() throws Exception {
+    public void shouldOpenMyActivity() throws Exception {
         String appName = new MyActivity().getResources().getString(R.string.app_name);
 //        assertThat(appName, equalTo("MyActivity"));
+
+        Assert.assertEquals(appName, "Hello, Android");
+    }
+
+    @Test
+    public void shouldSetTextWhenButtonClicked() throws Exception {
+        String appName = new MyActivity().getResources().getString(R.string.app_name);
+        String textfield = new MyActivity().getResources().getString(R.id.myTextView);
 
         Assert.assertEquals(appName, "Hello, Android");
     }
