40b18252e72e62493f29f76e8c82dffe8f27ecd0
keithnlarsen <keithnlarsen@gmail.com>
2011-10-06 17:48:46 -0600

    Tweeking the example test just a little so its a better example to follow.

--- src/test/java/ca/shawmedia/globaltv/MyActivityTest.java
+++ src/test/java/ca/shawmedia/globaltv/MyActivityTest.java
@@ -4,37 +4,54 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
-import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
 @RunWith(RobolectricTestRunner.class)
 public class MyActivityTest {
     MyActivity_ _activity;
+    TextView _textView;
+    Button _button;
+    EditText _editText;
 
     @Before
     public void setup() throws Exception {
         _activity = new MyActivity_();
         _activity.onCreate(null);
+
+        _textView = (TextView)_activity.findViewById(R.id.myTextView);
+        _button = (Button)_activity.findViewById(R.id.myButton);
+        _editText = (EditText)_activity.findViewById(R.id.myInput);
     }
 
     @Test
-    public void shouldOpenMyActivity() throws Exception {
-        String appName = _activity.getResources().getString(R.string.app_name);
-        Assert.assertEquals(appName, "Hello, Android");
+    public void should_Open_MyActivity_With_Default_Values() throws Exception {
+        // -- Setup --
+        String expectedText = _activity.getResources().getString(R.string.hello);
+
+        // -- Test --
+        String actualText = _textView.getText().toString();
+
+        // -- Verify --
+        assertThat(actualText, equalTo(expectedText));
     }
 
     @Test
-    public void shouldSetTextViewValueWhenButtonClicked() throws Exception {
-        TextView actualTextView = (TextView)_activity.findViewById(R.id.myTextView);
-        Button actualButton = (Button)_activity.findViewById(R.id.myButton);
-        EditText actualInput = (EditText)_activity.findViewById(R.id.myInput);
-        // Check that when the view starts up the text is set to the value defined in hello
-        Assert.assertEquals(_activity.getResources().getString(R.string.hello), actualTextView.getText());
-        // Set some text, and perform click and verify that the text changes in the TextView
-        actualInput.setText("testing");
-        actualButton.performClick();
-        Assert.assertEquals("Hello testing", actualTextView.getText());
+    public void should_Set_Text_View_Value_When_Button_Clicked() throws Exception {
+        // -- Setup --
+        String expectedText = "Hello testing";
+
+        // -- Test --
+        _editText.setText("testing");
+        _button.performClick();
+        String actualText = _textView.getText().toString();
+
+        // -- Verify --
+        assertThat(actualText, equalTo(expectedText));
     }
 }
